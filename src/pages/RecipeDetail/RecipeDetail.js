333d11b543d4cb48e028f788a9121c0d6c72e943
Guyon Maissan <g.maissan@Amega.nl>
2022-07-04 09:37:48 +0200

    first setup recipedetailpage #3

--- src/pages/RecipeDetail/RecipeDetail.js
+++ src/pages/RecipeDetail/RecipeDetail.js
@@ -1,9 +1,69 @@
-
+import styles from "./RecipeDetail.module.css"
+import food from "../../assets/juicy-steak-medium-rare-beef-with-spices-grilled-vegetables.jpg"
+import addIcon from "../../assets/add.png"
+import minusIcon from "../../assets/minus.png"
+import {useState} from "react";
 
 function RecipeDetail(){
-    return(
+    const [countPerson, setCountPerson] = useState(2);
 
+    return(
         <>
+            <section>
+                <article className={styles[`article-container`]}>
+                    <div className={styles[`article-content`]}>
+                        <h2>Titel</h2>
+                        <p>Bereidingstijd:</p>
+                        <p>soortkeuken</p>
+                        <p>soortGerecht</p>
+                    </div>
+                    <img className={styles[`img`]} src={food} alt="food"/>
+                </article>
+                <article className={styles[`article-container`]}>
+                    <div className={styles[`ingredient-content`]}>
+                        <div className={styles[`person-counter-container`]}>
+                            <p>{countPerson} personen</p>
+                            <button
+                                type="button"
+                                className={styles[`round-button`]}
+                                name="minus-button"
+                                onClick={() => setCountPerson(countPerson - 1)}
+                            >
+                                <img className={styles[`add-minus-icon`]} src={minusIcon} alt="minus"/>
+                            </button>
+                            <button
+                                type="button"
+                                className={styles[`round-button`]}
+                                name="add-button"
+                                onClick={() => setCountPerson(countPerson + 1)}
+                            >
+                                <img className={styles[`add-minus-icon`]} src={addIcon} alt="add"/>
+                            </button>
+                        </div>
+                        <ul className={styles[`list-container`]}>
+                            <li>....</li>
+                            <li>....</li>
+                            <li>....</li>
+                            <li>....</li>
+                            <li>....</li>
+                            <li>....</li>
+                            <li>....</li>
+                            <li>etc...</li>
+
+                        </ul>
+                    </div>
+                    <div className={styles[`food-prepare-container`]}>
+                        <h2>Bereidingswijze</h2>
+                        <ul className={styles[`list-container`]}>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, illum!</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, rem!</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum dolorem harum non, provident rem saepe sunt unde. Deleniti dolores eius modi nostrum quod tenetur. A at cum cumque, dolores facere hic illo illum non, nostrum obcaecati, quas quia sequi.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat fugit impedit iste iure quisquam? Doloribus eligendi facilis illo nihil odit!</li>
+
+                        </ul>
+                    </div>
+                </article>
+            </section>
         </>
 
     );
